558ab3f9b9eaeff750136e6337676682c37b30ab
harshdhiman-dev <harsh.dhiman@asituj.com>
2026-06-30 13:39:50 +0530

    Fix index.js

--- themes/digitalsilk/build/react/blocks/custom/dst-event-fares/index.asset.php
+++ themes/digitalsilk/build/react/blocks/custom/dst-event-fares/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-primitives'), 'version' => '142159e5f051e13af072');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-primitives'), 'version' => 'ea1cddfca222d0ccb1e5');
--- themes/digitalsilk/modules/gutenberg/blocks/custom/dst-event-fares/index.js
+++ themes/digitalsilk/modules/gutenberg/blocks/custom/dst-event-fares/index.js
@@ -2,6 +2,9 @@ import { registerBlockType } from '@wordpress/blocks';
 import { BlockEdit } from './edit';
 import metadata from './block.json';
 
+import './style.scss';
+import './editor.scss';
+
 registerBlockType( metadata.name, {
 	edit: BlockEdit,
 	save: () => null,
