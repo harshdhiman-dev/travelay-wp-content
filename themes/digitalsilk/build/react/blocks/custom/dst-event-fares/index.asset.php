011e496113783526f3b607e74175d145e4caf971
harshdhiman-dev <harsh.dhiman@asituj.com>
2026-06-30 14:35:34 +0530

    revert

--- themes/digitalsilk/build/react/blocks/custom/dst-event-fares/index.asset.php
+++ themes/digitalsilk/build/react/blocks/custom/dst-event-fares/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-primitives'), 'version' => '709f0b28e03416d85f01');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-primitives'), 'version' => '2228bb7a7bf164e3b8a3');
