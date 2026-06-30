cc35ad330285155ff2f7daa70c10a1bc91884d3c
harshdhiman-dev <harsh.dhiman@asituj.com>
2026-06-30 14:50:03 +0530

    add city fixes

--- themes/digitalsilk/build/react/blocks/custom/dst-event-fares/index.asset.php
+++ themes/digitalsilk/build/react/blocks/custom/dst-event-fares/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-primitives'), 'version' => '2228bb7a7bf164e3b8a3');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-primitives'), 'version' => '2e40fdd36b0531ff84f5');
